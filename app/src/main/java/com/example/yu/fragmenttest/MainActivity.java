1343e6fbb7d422e32595d00c01bdf782f083c784
yuluo-ding <luoshatter@gmail.com>
2015-05-22 09:34:13 +0800

    last

--- app/src/main/java/com/example/yu/fragmenttest/MainActivity.java
+++ app/src/main/java/com/example/yu/fragmenttest/MainActivity.java
@@ -1,18 +1,39 @@
 package com.example.yu.fragmenttest;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.button:
+                        AnotherRightFragment fragment = new AnotherRightFragment();
+                        FragmentManager fragmentManager = getFragmentManager();
+                        android.app.FragmentTransaction transaction = fragmentManager.beginTransaction();
+                        transaction.replace(R.id.right_layout, fragment);
+                        transaction.commit();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
     }
 
     @Override
